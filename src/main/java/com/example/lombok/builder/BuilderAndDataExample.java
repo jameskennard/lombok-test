d1eb6c42836b1b39ef5fed7b417378165ac15f9d
James Kennard <james.kennard@yellowdog.ai>
2026-03-06 15:39:32 +0000

    Add more examples

---
+++ src/main/java/com/example/lombok/builder/BuilderAndDataExample.java
@@ -0,0 +1,12 @@
+package com.example.lombok.builder;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class BuilderAndDataExample {
+
+    private int parentField;
+
+}
---
+++ src/main/java/com/example/lombok/builder/BuilderAndDataExampleWithParent.java
@@ -0,0 +1,10 @@
+package com.example.lombok.builder;
+
+///  You cannot do this, unless the parent class has a noargs constructor
+//@Builder
+//@Data
+//public class BuilderAndDataExampleWithParent extends BuilderAndDataExample {
+//
+//    private int childField;
+//
+//}
